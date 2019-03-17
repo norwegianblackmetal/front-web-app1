7e8a1e49b5bc9142195280c9c571d9db2bbfcdb7
Maxim Finozhenok <finojenok.m@gmail.com>
2019-03-17 23:32:10 +0300

    Created web-app on Python and Flask

---
+++ Main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def main():
+    print(hello())
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
