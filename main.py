d93531e13ecd8e0adcc58b0b40175cef36864ea6
Maxim Finozhenok <finojenok.m@gmail.com>
2020-09-25 19:18:58 +0300

    add html

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def endpoint1():
-    return 'Yanka seksi!'
+    return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='127.0.0.1', port='5000')
