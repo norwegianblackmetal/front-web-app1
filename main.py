ba5c0f0f25ce77bbff2b9ed7408c641c9efa5f0b
Maxim Finozhenok <finojenok.m@gmail.com>
2020-09-25 18:43:46 +0300

    ad

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def endpoint1():
-    return 'Hollow water!'
+    return 'Yanka seksi!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
