e45d6b5adae1d410dd0297e400d9453bebfb11f9
Maxim Finozhenok <finojenok.m@gmail.com>
2020-10-11 20:19:45 +0300

    add js

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def endpoint1():
     return render_template('base.html')
 
+@app.route('/str')
+def str():
+    return "strinfg"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
